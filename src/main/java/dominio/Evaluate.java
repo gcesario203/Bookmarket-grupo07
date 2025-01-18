c5331ba20158a5ed4c216c57e7c52d9a08e68497
gcesario203 <gcesario203@gmail.com>
2025-01-18 18:41:11 -0300

    refact: Adição de id para avaliação

--- src/main/java/dominio/Evaluate.java
+++ src/main/java/dominio/Evaluate.java
@@ -2,6 +2,7 @@ package dominio;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.UUID;
 
 /*
  * Classe que possui a avaliação de um item de pedido de algum livro adquirido por um cliente
@@ -9,21 +10,42 @@ import java.io.Serializable;
 public class Evaluate implements Serializable {
 	private static final long serialVersionUID = -4063511252485472431L;
 	
+	private final UUID id;
+	
 	private final Customer customer;
 	
 	private final Book book;
 	
 	private final int value;
 	
-	public Evaluate(Customer customer, Book book, int value) throws IOException
+	public Evaluate(UUID id, Customer customer, Book book, int value) throws IOException
 	{
+		this.id = id;
+		
 		this.customer = customer;
 		
 		this.book = book;
 		
 		this.value = value;
 		
-		this.ValidateEvaluateValue();
+		this.validateEvaluateValue();
+	}
+	
+	public Evaluate(Customer customer, Book book, int value) throws IOException
+	{
+		this.id = UUID.randomUUID();
+		
+		this.customer = customer;
+		
+		this.book = book;
+		
+		this.value = value;
+		
+		this.validateEvaluateValue();
+	}
+	
+	public string getId() {
+		return this.id;
 	}
 	
 	public int getValue() {
@@ -39,7 +61,7 @@ public class Evaluate implements Serializable {
 	}
 	
 	
-	private void ValidateEvaluateValue() throws IOException {
+	private void validateEvaluateValue() throws IOException {
 		if(this.value < 0 || this.value > 5)
 			throw new IOException("Avaliação com valor inválido");
 	}
