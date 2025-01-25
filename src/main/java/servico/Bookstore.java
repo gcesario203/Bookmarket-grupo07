231707801dc62265e30e2adf51f3d6eb94065fdf
Douglas019BR <douglassermarini@gmail.com>
2025-01-25 12:40:45 -0300

    changing constants from populateCountries() to new file Constants.java

--- src/main/java/servico/Bookstore.java
+++ src/main/java/servico/Bookstore.java
@@ -15,6 +15,7 @@ import dominio.Order;
 import dominio.OrderLine;
 import dominio.Review;
 import dominio.Stock;
+import util.Constants.*;
 import util.TPCW_Util;
 
 import java.io.IOException;
@@ -34,6 +35,8 @@ import java.util.Optional;
 import java.util.Random;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
+import static util.Constants.*;
 
 /**
  * Descrição da Arquitetura do Bookstore
@@ -926,77 +929,14 @@ public class Bookstore implements Serializable {
      * consumidor.
      */
     private static void populateCountries() {
-        String[] countries = {"United States", "United Kingdom", "Canada",
-            "Germany", "France", "Japan", "Netherlands",
-            "Italy", "Switzerland", "Australia", "Algeria",
-            "Argentina", "Armenia", "Austria", "Azerbaijan",
-            "Bahamas", "Bahrain", "Bangla Desh", "Barbados",
-            "Belarus", "Belgium", "Bermuda", "Bolivia",
-            "Botswana", "Brazil", "Bulgaria", "Cayman Islands",
-            "Chad", "Chile", "China", "Christmas Island",
-            "Colombia", "Croatia", "Cuba", "Cyprus",
-            "Czech Republic", "Denmark", "Dominican Republic",
-            "Eastern Caribbean", "Ecuador", "Egypt",
-            "El Salvador", "Estonia", "Ethiopia",
-            "Falkland Island", "Faroe Island", "Fiji",
-            "Finland", "Gabon", "Gibraltar", "Greece", "Guam",
-            "Hong Kong", "Hungary", "Iceland", "India",
-            "Indonesia", "Iran", "Iraq", "Ireland", "Israel",
-            "Jamaica", "Jordan", "Kazakhstan", "Kuwait",
-            "Lebanon", "Luxembourg", "Malaysia", "Mexico",
-            "Mauritius", "New Zealand", "Norway", "Pakistan",
-            "Philippines", "Poland", "Portugal", "Romania",
-            "Russia", "Saudi Arabia", "Singapore", "Slovakia",
-            "South Africa", "South Korea", "Spain", "Sudan",
-            "Sweden", "Taiwan", "Thailand", "Trinidad",
-            "Turkey", "Venezuela", "Zambia"};
+            System.out.print("Creating " + COUNTRIES.length + " countries...");
 
-        double[] exchanges = {1, .625461, 1.46712, 1.86125, 6.24238, 121.907,
-            2.09715, 1842.64, 1.51645, 1.54208, 65.3851,
-            0.998, 540.92, 13.0949, 3977, 1, .3757,
-            48.65, 2, 248000, 38.3892, 1, 5.74, 4.7304,
-            1.71, 1846, .8282, 627.1999, 494.2, 8.278,
-            1.5391, 1677, 7.3044, 23, .543, 36.0127,
-            7.0707, 15.8, 2.7, 9600, 3.33771, 8.7,
-            14.9912, 7.7, .6255, 7.124, 1.9724, 5.65822,
-            627.1999, .6255, 309.214, 1, 7.75473, 237.23,
-            74.147, 42.75, 8100, 3000, .3083, .749481,
-            4.12, 37.4, 0.708, 150, .3062, 1502, 38.3892,
-            3.8, 9.6287, 25.245, 1.87539, 7.83101,
-            52, 37.8501, 3.9525, 190.788, 15180.2,
-            24.43, 3.7501, 1.72929, 43.9642, 6.25845,
-            1190.15, 158.34, 5.282, 8.54477, 32.77, 37.1414,
-            6.1764, 401500, 596, 2447.7};
+            for (int i = 0; i < COUNTRIES.length; i++) {
+                createCountry(COUNTRIES[i], CURRENCIES[i], EXCHANGES[i]);
+            }
 
-        String[] currencies = {"Dollars", "Pounds", "Dollars", "Deutsche Marks",
-            "Francs", "Yen", "Guilders", "Lira", "Francs",
-            "Dollars", "Dinars", "Pesos", "Dram",
-            "Schillings", "Manat", "Dollars", "Dinar", "Taka",
-            "Dollars", "Rouble", "Francs", "Dollars",
-            "Boliviano", "Pula", "Real", "Lev", "Dollars",
-            "Franc", "Pesos", "Yuan Renmimbi", "Dollars",
-            "Pesos", "Kuna", "Pesos", "Pounds", "Koruna",
-            "Kroner", "Pesos", "Dollars", "Sucre", "Pounds",
-            "Colon", "Kroon", "Birr", "Pound", "Krone",
-            "Dollars", "Markka", "Franc", "Pound", "Drachmas",
-            "Dollars", "Dollars", "Forint", "Krona", "Rupees",
-            "Rupiah", "Rial", "Dinar", "Punt", "Shekels",
-            "Dollars", "Dinar", "Tenge", "Dinar", "Pounds",
-            "Francs", "Ringgit", "Pesos", "Rupees", "Dollars",
-            "Kroner", "Rupees", "Pesos", "Zloty", "Escudo",
-            "Leu", "Rubles", "Riyal", "Dollars", "Koruna",
-            "Rand", "Won", "Pesetas", "Dinar", "Krona",
-            "Dollars", "Baht", "Dollars", "Lira", "Bolivar",
-            "Kwacha"};
-
-        System.out.print("Creating " + countries.length + " countries...");
-
-        for (int i = 0; i < countries.length; i++) {
-            createCountry(countries[i], currencies[i], exchanges[i]);
+            System.out.println(" Done");
         }
-
-        System.out.println(" Done");
-    }
 
     private static void populateAddresses(int number, Random rand) {
         System.out.print("Creating " + number + " addresses...");
